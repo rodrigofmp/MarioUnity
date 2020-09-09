88de3c7c4b7ffae0f0db6e766dee554d258fc10b
Rodrigo Petters <rodrigofmp@gmail.com>
2020-09-08 21:01:30 -0300

    Coin box criado

---
+++ Assets/Scripts/CoinBox.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinBox : MonoBehaviour
+{
+    Animator _animator;
+    AudioSource _audio;
+
+    public bool IsActive = false;
+    public float SoundDelay = 0.25f;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _audio = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.CompareTag("Mario") && !IsActive)
+        {
+            IsActive = true;
+            _animator.SetBool("Active", IsActive);
+            _audio.PlayDelayed(SoundDelay);
+            GameObject.Find("Main Camera").GetComponent<GameBehaviour>().AddCoin();
+        }
+    }
+}
