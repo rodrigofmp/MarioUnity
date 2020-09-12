3e235c5f9073738f24bd0641fcc112c8337a8041
Rodrigo Petters <rodrigofmp@gmail.com>
2020-09-12 18:41:01 -0300

    Koopa walking

---
+++ Assets/Scripts/KoopaTroopa.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KoopaTroopa : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    private Animator _animator;
+
+    public int Direction = -1;
+    public float Velocity = 1f;
+    public bool IsAlive = true;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (IsAlive)
+        {
+            _rb.velocity = new Vector2(Velocity * Direction, _rb.velocity.y);
+        }
+
+        _animator.SetFloat("Velocity", Mathf.Abs(_rb.velocity.x));
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Platform") && IsAlive)
+        {
+            _rb.velocity = new Vector2(0, _rb.velocity.y);
+
+            Direction = -Direction;
+            this.transform.localScale = new Vector3(
+                this.transform.localScale.x * -1, 
+                this.transform.localScale.y, 
+                this.transform.localScale.z);
+        }
+    }
+}
